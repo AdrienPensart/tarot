72f46df5cbc953e4fd844243d5c46beb6e3a7402
Adrien Pensart <adrien.pensart@corp.ovh.com>
2016-07-24 12:03:53 -0400

    combinations first usage

--- tarot.d
+++ tarot.d
@@ -10,15 +10,15 @@ auto Petit = Carte(Couleur.Atout, 1);
 auto Excuse = Carte(Couleur.Atout, 0);
 
 string declare(ubyte v){
-	return "auto _" ~ to!string(v) ~ " = Carte(Couleur.Atout, " ~ to!string(v) ~ ");";
+    return "auto _" ~ to!string(v) ~ " = Carte(Couleur.Atout, " ~ to!string(v) ~ ");";
 }
 
 string declareAll(){
-	string all;
-	foreach(ubyte i; 1..22){
-		all ~= declare(i);
-	}
-	return all;
+    string all;
+    foreach(ubyte i; 1..22){
+        all ~= declare(i);
+    }
+    return all;
 }
 
 mixin(declareAll());
@@ -35,53 +35,66 @@ struct Carte
     Couleur c;
     ubyte v;
 
+    invariant{
+        assert(c == Couleur.Pique || c == Couleur.Carreau || c == Couleur.Coeur || c == Couleur.Trefle || c == Couleur.Atout);
+        if(c == Couleur.Atout){
+            assert(v >= 0 && v <= 21);
+        } else {
+            assert(v >= 0 && v <= 14);
+        }
+    }
+
     public pure @safe nothrow{
-		bool master(Carte arg){
-	        if(c == arg.c){
-    	        return v > arg.v;
-        	}
-	        return c == Couleur.Atout;
-    	}
+        bool master(Carte arg){
+            if(c == arg.c){
+                return v > arg.v;
+            }
+            return c == Couleur.Atout;
+        }
 
-	    bool discardable(bool force=false){
-    	    if(c == Couleur.Atout && v > 1 && v < 21){
-        	    if(force){
-            	    return true;
-	            }
-    	    }
-        	else if(v != 14){
-            	return true;
-	        }
-    	    return false;
-	    }
+        bool discardable(bool force=false){
+            if(c == Couleur.Atout){
+                if(v == 0 || v == 1 || v == 21){
+                    return false;
+                }
+                if(force){
+                    return true;
+                }
+            }
+            else if(v != 14){
+                return true;
+            }
+            return false;
+        }
 
-		float points(){
-			if (c == Couleur.Atout && (v == 0 || v == 1 || v == 21)){
-				return 4.5;
-			}
-			if (v == 14){
-				return 4.5;
-			}
-			if (v == 13){
-				return 3.5;
-			}
-			if (v == 12){
-				return 2.5;
-			}
-			if (v == 11){
-				return 1.5;
-			}
-			return 0.5;
-		}
-	}
+        float points(){
+            if (c == Couleur.Atout && (v == 0 || v == 1 || v == 21)){
+                return 4.5;
+            }
+            if (v == 14){
+                return 4.5;
+            }
+            if (v == 13){
+                return 3.5;
+            }
+            if (v == 12){
+                return 2.5;
+            }
+            if (v == 11){
+                return 1.5;
+            }
+            return 0.5;
+        }
+    }
 }
 
 alias Carte[] Deck;
 
-void show(Deck deck){
+auto show(Deck deck){
     foreach(d; deck){
-        writeln("[",d.v,",",d.c,"]");
+        write("[",d.v,",",d.c,"]");
     }
+    return deck;
 }
 
 bool petitSec(Deck deck){
@@ -140,25 +153,25 @@ void main() {
             }
         }
 
-        while(cards.length > 18){
-            foreach(index, card; cards){
-                if(card.discardable(true)){
-                    discards ~= card;
-                    cards = cards[0..index] ~ cards[index+1..cards.length];
-                    break;
+        if(cards.length > 18){
+            writeln("Exceptional situation, we need to choose between outsiders to create discard stack");
+            while(cards.length > 18){
+                foreach(index, card; cards){
+                    if(card.discardable(true)){
+                        discards ~= card;
+                        cards = cards[0..index] ~ cards[index+1..cards.length];
+                        break;
+                    }
                 }
             }
         }
 
-        while(cards.length < 18){
-            foreach(index, card; discards){
-            }
+        writeln("Discards:", discards);
+        writeln("Cards:", cards);
+        if(cards.length < 18){
+            writeln("Computing combinations: ", 24 - discards.length);
+            discards.combinations!false(24 - discards.length).map!(x => x).writeln;
         }
-
-        writeln("Discard :");
-        discards.show();
-        writeln("Jeu du preneur : ");
-        cards.show();
         break;
     }
 }
@@ -169,6 +182,10 @@ unittest {
     Deck petit = [Petit];
     Deck excuse = [Excuse];
 
+    assert(Excuse.discardable() == false);
+    assert(Petit.discardable() == false);
+    assert(_21.discardable() == false);
+
     assert(petitSec(misere) == false);
     assert(petitSec(misere ~ excuse) == false);
     assert(petitSec(misere ~ petit) == true);
@@ -178,8 +195,8 @@ unittest {
     assert(petitSec(misere ~ atouts ~ petit ~ excuse) == false);
     assert(petitSec(misere ~ atouts ~ excuse) == false);
 
-	assert(equal(misere.combinations!false(2), misere.combinations(2)));
-	assert(equal(misere.combinations!true(2), misere.combinations(2)));
-	assert(equal(misere.combinations!false(2), misere.combinations!true(2)));
+    assert(equal(misere.combinations!false(2), misere.combinations(2)));
+    assert(equal(misere.combinations!true(2), misere.combinations(2)));
+    assert(equal(misere.combinations!false(2), misere.combinations!true(2)));
 }
 
